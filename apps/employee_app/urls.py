d9be62f1fba2ce5d606c4ad1e45eca0ad8dfc536
dickanirwansyah <dickanirwansyah@gmail.com>
2018-06-11 23:58:25 +0700

    menambahkan sesuatu di file  urls.py

--- apps/employee_app/urls.py
+++ apps/employee_app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^add_employee', views.add_new_employee)
 ]
 
--- apps/employee_app/views.py
+++ apps/employee_app/views.py
@@ -2,8 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import employee
+
 
 # Create your views here. sama aja seperti controller pada spring-boot
 
 def index(request):
-    return render(request, 'employee_app/index.html')
+    employees = employee.objects.all()
+    context = {'employees':employees}
+    return render(request, 'employee_app/index.html', context)
+
+def add_new_employee(request):
+    return render(request, 'employee_app/add_employee.html')
+
+
