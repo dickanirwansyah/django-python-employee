fd20c9c736df3279869035f0fd02089855c1bab4
dickanirwansyah <dickanirwansyah@gmail.com>
2018-06-12 14:38:46 +0700

    commit employee

--- apps/employee_app/urls.py
+++ apps/employee_app/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns =[
     url(r'^$', views.index),
-    url(r'^add_employee', views.add_new_employee)
+    url(r'^add_employee', views.add_new_employee),
+    url(r'^save_employee', views.save_employee),
+    url(r'^get_detail_employee/(?P<id>\d+)$', views.get_employee),
+    url(r'^update_employee/(?P<id>\d+)$', views.update_employee),
+    url(r'^delete_employee/(?P<id>\d+)$', views.destroy_employee)
 ]
 
--- apps/employee_app/views.py
+++ apps/employee_app/views.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from models import employee
 
 
-# Create your views here. sama aja seperti controller pada spring-boot
+# views controller
 
 def index(request):
     employees = employee.objects.all()
@@ -15,4 +15,33 @@ def index(request):
 def add_new_employee(request):
     return render(request, 'employee_app/add_employee.html')
 
+def get_employee(request, id):
+    emp = employee.objects.get(id=id)
+    context = {"emp":emp}
+    return render(request, 'employee_app/update_employee.html', context)
+
+def destroy_employee(request, id):
+    emp = employee.objects.get(id = id)
+    emp.delete()
+    return redirect('/')
+
+def save_employee(request):
+    print request.POST
+    emp = employee(
+        name_employee = request.POST['name_employee'],
+        email_employee = request.POST['email_employee'],
+        position_employee = request.POST['position_employee'],
+        ages_employee = request.POST['ages_employee']
+    )
+    emp.save()
+    return redirect('/')
+
+def update_employee(request, id):
+    emp = employee.objects.get(id=id)
+    emp.name_employee= request.POST['name_employee']
+    emp.email_employee = request.POST['email_employee']
+    emp.position_employee = request.POST['position_employee']
+    emp.ages_employee = request.POST['ages_employee']
+    emp.save()
+    return redirect('/')
 
